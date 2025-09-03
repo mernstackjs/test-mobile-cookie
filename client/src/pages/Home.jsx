edb7449029cc30bc595097866f0f2152ff6fa0c4
learncodewithahmed <learncodewithahmed@example.com>
2025-09-03 12:53:32 +0200

    i made some change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
   const [data, setData] = useState(null)
+  const [time, setTime] = useState(null)
+  const showTime = () => {
+    setTime(new Date().toLocaleTimeString())
+  }
   console.log(import.meta.env.VITE_API_URL )
   const testData = async () => {
     const response = await fetch(`${import.meta.env.VITE_API_URL}`)
     const data = await response.json()
+    console.log("data", data)
+    console.log("--------------------------------")
+    console.log("--------------------------------")
     console.log("message", data?.message)
+    console.log("--------------------------------")
     console.log("userAgent", data?.userAgent)
+    console.log("--------------------------------")
+    console.log("-----------i made some changes------------")
     setData(data)
   }
   testData()
@@ -16,6 +26,20 @@ export default function Home() {
       <h1 className='text-2xl font-bold'>Home</h1>
       <p>{data?.message}</p>
       <p>{data?.userAgent}</p>
+
+      <p>i made some changes</p>
+
+      <span>Today is {new Date().toLocaleDateString()}</span>
+
+      <button onClick={testData}>Test Data</button>
+      <button onClick={() => {
+    console.log("this is a game changer")
+      }}>Game Changer</button>
+
+      <h1>This Year is very important for me</h1>
+
+      <button onClick={showTime}>Show Time</button>
+      <p>{time}</p>
     </div>
   )
 }
