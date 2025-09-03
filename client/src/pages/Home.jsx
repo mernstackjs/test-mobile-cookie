b7da65167cdaa245c09b49fe0b4440ad32061f19
learncodewithahmed <learncodewithahmed@example.com>
2025-09-03 12:47:32 +0200

    display data into home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 export default function Home() {
+  const [data, setData] = useState(null)
   console.log(import.meta.env.VITE_API_URL )
   const testData = async () => {
     const response = await fetch(`${import.meta.env.VITE_API_URL}`)
     const data = await response.json()
     console.log(data)
+    setData(data)
   }
   testData()
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <p>{data?.message}</p>
+      <p>{data?.userAgent}</p>
+    </div>
   )
 }
